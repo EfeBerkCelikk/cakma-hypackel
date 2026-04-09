1c4ccf39258f7aa74c0e1ef2eaf074dfae373668
EfeCelikk <Efeberk43@hotmail.com>
2026-04-09 18:59:37 +0300

    Add filterGames function to filter game list

---
+++ script.js
@@ -0,0 +1,16 @@
+function filterGames() {
+    let input = document.getElementById('searchInput');
+    let filter = input.value.toLowerCase();
+    let ul = document.getElementById("gameList");
+    let li = ul.getElementsByTagName('li');
+
+    for (let i = 0; i < li.length; i++) {
+        let a = li[i].getElementsByTagName("a")[0];
+        let txtValue = a.textContent || a.innerText;
+        if (txtValue.toLowerCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
